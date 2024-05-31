7b251a46c56e92b60254169552e1ae2d93fe86bd
Milan Hingu <139786280+milllan10@users.noreply.github.com>
2024-05-31 18:26:54 +0530

    Update submit_form.php

--- submit_form.php
+++ submit_form.php
@@ -8,7 +8,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                    
     $mail->SMTPAuth   = true;                                
     $mail->Username   = 'milanhingu9987@gmail.com';          
-    $mail->Password   = 'mulikwqfhelymrge';                  
+    $mail->Password   = '';                  
     $mail->SMTPSecure = 'tls';                               
     $mail->Port       = 587;                                 
     $mail->setFrom('milanhingu9987@gmail.com', 'The Super Construction');
